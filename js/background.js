c66ab15d1d16781c097bcd3a6d9a5e42e9391a5b
cover1sea <gappuri@cover1sea.net>
2018-02-09 19:01:53 +0900

    xmlパース処理を改善

--- js/background.js
+++ js/background.js
@@ -12,34 +12,43 @@ setInterval(function(){
   );
   liveCount = 0;
   var live = [];
-  var liveThumb = [];
   request.open("GET", rss, true);
   request.onreadystatechange = function(){
     if(request.readyState == 4 && request.status == 200){
-      for(i=0; i<userList.length; i++){
-        if(userList[i]){
-          if(request.responseText.indexOf("<name><![CDATA["+userList[i])!=-1){
+      //GET Success
+      
+      xmldoc =request.responseXML;
+      entry = xmldoc.getElementsByTagName("entry");
+      for(i=0; i<entry.length; i++){
+        streamInfo={
+          title:        entry[i].getElementsByTagName("title")[0].textContent, 
+          user:         entry[i].getElementsByTagName("name")[0].textContent,
+          thumb:        entry[i].getElementsByTagName("ct:thumbnail_path")[0].textContent,
+          numListeners: entry[i].getElementsByTagName("ct:listener")[0].textContent
+        };
+        console.log(streamInfo);
+        for(j=0; j<userList.length; j++){
+        if(userList[j]){
+          if(userList[j] === streamInfo.user){
             /* Live */
-            live.push(userList[i]);
+            if(streamInfo.thumb === "http:/img/no_thumbnail_image.png"){
+              //For No Image streamer
+              streamInfo.thumb = "img/no_profile_image.png";
+            }
+            live.push(streamInfo);
             liveCount++;
             
-            /* get thumbnail */
-            search = new RegExp("http://.*" + userList[i] + ".*.png");
-            thumb = request.responseText.match(search);
-            if(!thumb){
-              thumb = "img/no_profile_image.png";
-            }
-            liveThumb.push(thumb);
           }
         }
+      }
       }
       chrome.browserAction.setBadgeText({"text": String(liveCount)});
       chrome.storage.sync.set({
         liveList: live,
-        thumbnail: liveThumb
       },function(){}
       );
     }
   };
   request.send(null);
 },10000);
+
--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 chrome.storage.sync.get({
     liveList: "",
-    thumbnail: ""
   },
   function(items){
     for(i=0; i<items.liveList.length; i++){
-      text = document.getElementById("live").innerHTML;
-      document.getElementById("live").innerHTML = text + '<li><a href="https://www.cavelis.net/live/' + items.liveList[i] + '" target="_blank"><img src="' + items.thumbnail[i] + '" width="60px" height="auto" align="middle" />' +items.liveList[i] + "</a></li>";
+      text = document.getElementById("live");
+      text.innerHTML = text.innerHTML + '<li><a href="https://www.cavelis.net/live/' + items.liveList[i].user + '" target="_blank"> </a><div class="popup_image"><img src="' + items.liveList[i].thumb + '" width="70px" height="auto" align="middle" /></div><div class="popup_text"><p class="streamer">' +items.liveList[i].user + '</p><p class="info">' + items.liveList[i].title + ' - ' + items.liveList[i].numListeners + ' listeners</p></div><div class="popup_dummy"></div></li>';
     }
   }
 );
